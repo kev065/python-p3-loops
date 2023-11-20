578d6f9e3c1b36fd9928899d827fbebd5543a290
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-20 14:02:48 +0300

    1st test passed

--- lib/looping.py
+++ lib/looping.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -=1
+        print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
