dde0cfb2c16f6ce109c95e53b0f090477cd5a96c
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-20 14:06:16 +0300

    2 of 3 passing

--- lib/looping.py
+++ lib/looping.py
@@ -7,9 +7,16 @@ def happy_new_year():
         i -=1
         print("Happy New Year!")
 
+
+
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = []
+    for num in int_list:
+        squared = num * num
+        squared_list.append(squared)
+    return squared_list
+
+
 
 def fizzbuzz():
     # code goes here!
