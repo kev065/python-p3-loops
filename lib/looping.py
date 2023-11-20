3341110696771201cedcf742b3eb545df845dbf4
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-20 14:26:58 +0300

    all tests passed

--- lib/looping.py
+++ lib/looping.py
@@ -19,5 +19,12 @@ def square_integers(int_list):
 
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0: # starts a for loop that iterates over the numbers from 1 to 100
+            print("FizzBuzz") 
+        elif number % 3 == 0:
+            print("Fizz") 
+        elif number % 5 == 0:
+            print("Buzz") 
+        else:
+            print(number) 
